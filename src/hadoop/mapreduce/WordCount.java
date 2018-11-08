2aafd45ccbad84f960f1a20705620b9b45ed4c11
foton <foton@01JRFWB675>
2018-11-08 16:27:58 +0800

    fix

--- src/hadoop/mapreduce/WordCount.java
+++ src/hadoop/mapreduce/WordCount.java
@@ -3,6 +3,7 @@ package hadoop.mapreduce;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -15,7 +16,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WordCount {
 
-	public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	public static class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
 		@Override
 		protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
@@ -28,13 +29,13 @@ public class WordCount {
 
 	}
 
-	public class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	public static class WordCountReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
 		@Override
 		protected void reduce(Text key, Iterable<IntWritable> values,
 				Reducer<Text, IntWritable, Text, IntWritable>.Context context)
 				throws IOException, InterruptedException {
 			int count = 0;
-			
+
 			for (IntWritable value : values) {
 				count += value.get();
 			}
@@ -42,27 +43,33 @@ public class WordCount {
 		}
 
 	}
-	
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-		
-		System.out.println(args.length);
-		
+
+		System.out.println("[INFO] args.length = " + args.length);
+
 		Configuration conf = new Configuration();
+		conf.set("fs.defaultFS", "hdfs://192.168.0.23:8020");
+		FileSystem fileSystem = FileSystem.get(conf);
+
+		if (fileSystem.exists(new Path(args[1]))) {
+			fileSystem.delete(new Path(args[1]), true);
+		}
 		Job job = Job.getInstance(conf);
 		job.setJarByClass(WordCount.class);
-		
+
 		job.setMapperClass(WordCountMapper.class);
 		job.setReducerClass(WordCountReduce.class);
-		
+
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
-		
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
+
 		FileInputFormat.setInputPaths(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
+
 		job.waitForCompletion(true);
 	}
 }
